71ee2b424a8451dfe86c405ea156e492ec85d08b
cidiggs <charles.diggs0703@gmail.com>
2025-03-26 23:14:56 -0700

    Revert "Payments charles"

--- src/payments/routing.py
+++
@@ -1,9 +0,0 @@
- # payments/routing.py
-from django.urls import re_path
-from .consumers import PaymentStatusConsumer
-
-websocket_urlpatterns = [
-    re_path(r'^ws/payment-status/$', PaymentStatusConsumer.as_asgi()),
-]
-
-
