5ef92a03968f4688c40c6c90b8fc97885b5232c3
cidiggs <charles.diggs0703@gmail.com>
2025-03-31 20:15:23 -0700

    Update api.py

--- src/campaigns/api.py
+++ src/campaigns/api.py
@@ -1,19 +1,19 @@
 from typing import List
 from ninja import Router
-//from ninja_jwt.authentication import JWTAuth
+#from ninja_jwt.authentication import JWTAuth
 from django.shortcuts import get_object_or_404
 from .models import *
 from .schemas import *
 
 router = Router()
 
-//@router.get("", response = List[CampaignEntryListSchema], auth= JWTAuth())
+#@router.get("", response = List[CampaignEntryListSchema], auth= JWTAuth())
 @router.get("", response=List[CampaignEntryListSchema])
 def list_campaigns_entries(request):
     qs = CampaignEntry.objects.all()
     return qs
 
-//@router.get("{entry_id}/", response = CampaignEntryDetailSchema, auth= JWTAuth())
+#@router.get("{entry_id}/", response = CampaignEntryDetailSchema, auth= JWTAuth())
 @router.get("{entry_id}/", response=CampaignEntryDetailSchema)
 def get_campaign_entry(request, entry_id: int):
     obj = get_object_or_404(CampaignEntry, id=entry_id)
