59fa84031944b965102eb8039d59174f0cbf582c
Saul Serrano <saulserrano18@yahoo.com>
2025-04-12 20:14:36 -0700

    added pages, campagin create

--- src/campaigns/admin.py
+++ src/campaigns/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import CampaignEntry
+from .models import Campaign
 
 # Register your models here.
 
-admin.site.register(CampaignEntry)
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("title", "goal_amount", "current_amount", "creator", "start_date", "created_at")
+    search_fields = ("title", "description", "creator__username")
+    list_filter = ("start_date", "created_at")
