67a64038e9c24664427b98668cbd38af9625ea49
Saul Serrano <saulserrano18@yahoo.com>
2025-07-04 12:47:04 -0700

    changes to auth file, most things need to be fixed

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number' ]
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     wallet_address = models.CharField(max_length=255, blank=True, null=True)
     links = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
 
